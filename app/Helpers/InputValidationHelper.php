9702b61f5a378b0bcfb9fd485135ee1b0f3db181
mhmd303 <mohamadamer303@gmail.com>
2021-06-19 19:07:06 +0300

    initial commit

---
+++ app/Helpers/InputValidationHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class InputValidationHelper
+{
+
+    public function validateUserRegistration(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+    }
+}
